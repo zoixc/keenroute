295aafe597f47849ef16ef2dd2295a7d824dd90f
zoixc <113115585+zoixc@users.noreply.github.com>
2025-08-28 13:13:58 +0200

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,10 +1,37 @@
-const CACHE_NAME = "keenroute-cache-v1";
-const urlsToCache = ["/", "/static/manifest.json", "/static/css/style.css"];
+const CACHE_NAME = "keenroute-cache-v2"; // меняем версию, чтобы сбросить старый кеш
+const urlsToCache = [
+  "/", 
+  "/static/manifest.json",
+  "/static/css/style.css",
+  "/fonts/conquera-bold.ttf",
+  "/fonts/IBMPlexSans-Regular.ttf",
+  "/static/img/icon-72.png",
+  "/static/img/icon-192.png",
+  "/static/img/icon-512.png"
+];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
